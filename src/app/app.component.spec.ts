38836a79b0aa6f086304c3662b8497bc5408100f
Sandeep Soni <ssoni@sandeeps-imac.lan>
2018-09-10 22:19:49 -0500

    adding test for ag-grid

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { AgGridModule } from 'ag-grid-angular';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -12,7 +13,12 @@ describe('AppComponent', () => {
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
       imports: [AgGridModule.withComponents([])]
     }).compileComponents();
+    
   }));
+
+
+  
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -23,11 +29,41 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ag-grid-with-angular6');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ag-grid-with-angular6!');
-  }));
+
+  // ag-grid test
+  describe('AgGrid', () => {
+    it('should have expected column headers', () => {
+      let component: AppComponent;
+      let fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      const elm = fixture.nativeElement;
+      const grid = elm.querySelector('ag-grid-angular');
+      const headerCells = grid.querySelectorAll('.ag-header-cell-text');
+      const headerTitles = Array.from(headerCells).map((cell: any) =>
+          cell.textContent.trim()
+      );
+      expect(headerTitles).toEqual(['Make', 'Model', 'Price']);
+      });
   
+      it('first row should have expected data', () => {
+        let component: AppComponent;
+        let fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+  
+        const elm = fixture.nativeElement;
+        const grid = elm.querySelector('ag-grid-angular');
+        const firstRowCells = grid.querySelectorAll(
+            'div[row-id="0"] div.ag-cell-value'
+        );
+        const values = Array.from(firstRowCells).map((cell: any) =>
+            cell.textContent.trim()
+        );
+        console.log('values', values);
+        expect(values).toEqual(['Toyota', 'Celica', '35000']);
+       });
+     });
+  
+
 });
